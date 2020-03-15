78fdc596c5f03bad2e9388ac2e6c6a1d01c8f331
agiatdonguri <taiganishida2009@gmail.com>
2020-03-15 14:45:53 -0700

    Created Vertex

---
+++ OpenGLTest/Vertex.h
@@ -0,0 +1,12 @@
+#pragma once
+struct Vertex
+{
+	float x;
+	float y;
+	float z;
+	float u;
+	float v;
+
+	Vertex(float x, float y, float z, float u, float v) : x(x), y(y), z(z), u(u), v(v) {}
+};
+
