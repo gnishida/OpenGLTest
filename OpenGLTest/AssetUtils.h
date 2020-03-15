082bc94ee8bd5dea96fc819aaf13be9b79c5c36a
agiatdonguri <taiganishida2009@gmail.com>
2020-03-15 16:54:48 -0700

    Update AssetUtils.h

--- OpenGLTest/AssetUtils.h
+++ OpenGLTest/AssetUtils.h
@@ -7,7 +7,7 @@ class AssetUtils
 {
 public:
 	static std::vector<Vertex> createSphere(float radius);
-	//static std::vector<Vertex> createCube(float edge);
+	static std::vector<Vertex> createPrism(std::vector<std::pair<float, float>> polygon, float height);
 	static std::vector<Vertex> createRectangle(float width, float length);
 };
 
